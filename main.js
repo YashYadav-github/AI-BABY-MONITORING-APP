774a4820fbbb2cb29a1ea8679ccd39c07ffb022c
YashYadav-github <74818709+YashYadav-github@users.noreply.github.com>
2021-10-07 18:49:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function preload(){
+    alarm = loadSound("alarm.wav");
+}
+
+function setup(){
+    canvas = createCanvas(350, 350);
+    canvas.center();
+    // objectDetector = mlr.objectDetector()
+}
+
+function draw(){
+
+}
